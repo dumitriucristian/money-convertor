06d6fe6a6d47e0b03ea6ac6d6ff6abb2bc41fe7f
cdumitriu <cdumitriu@pentalog.com>
2021-05-21 11:10:44 +0300

    money converter with random image

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,85 @@
 import 'package:flutter/material.dart';
-
+//todo - create a stand alone widget for random image
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: "Convert your money",
+    home:MyApp(
+    )
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-         primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  int _random = 1;
+  String _amount = "0";
+  Image _image = Image.network('https://picsum.photos/200/300?random=1');
 
-    final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
+      final myController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
 
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-
-        title: Text(widget.title),
+        title:Text( "Some title"),
       ),
       body: Center(
         child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: _image,
+              ),
+            TextFormField(
+              controller: myController,
 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            ),
+            ElevatedButton(
+              onPressed: (){
+               setState(() {
+                 try {
+                  _random++;
+                   double num = double.parse( myController.text) * 2.99;
+                   _amount = num.toStringAsFixed(2);
+                   _image = Image.network('https://picsum.photos/500?random=$_random',
+                      width: double.infinity,
+                      fit: BoxFit.fill
+                   );
+
+                 } catch (e, s) {
+                   showDialog(context: context,
+                       builder: (BuildContext context) => AlertDialog(
+                         title: Text('Sorry mate, Only decimals allowed'),
+                         content: Text('Please use only nice decimals like 1.1'),
+                         actions: [
+                           TextButton(
+                             onPressed: () => Navigator.pop(context, 'Cancel'),
+                             child: Text('Ok - I will use only decimal'),
+                           ),
+                         ],
+                       )
+                   );
+                 }
+               });
+              },
+              child: Text('submit'),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              "$_amount",
+              style:TextStyle(fontWeight: FontWeight.bold, fontSize: 35)
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      )
     );
   }
 }
+
+
